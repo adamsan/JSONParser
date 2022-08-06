3199eccef391411e4faa735f6427028d3a9b884c
Szentirmai-Schon Adam <adamsan3@gmail.com>
2022-08-06 18:05:40 +0200

    JSON.parse can parse a json string

--- src/main/java/hu/adamsan/jsonparser/JSON.java
+++ src/main/java/hu/adamsan/jsonparser/JSON.java
@@ -3,4 +3,16 @@ package hu.adamsan.jsonparser;
 public class JSON {
     private JSON() {
     }
+
+    public static JSONString parser(String s) {
+        return new JSONString(s);
+    }
+
+    static class JSONString extends JSON {
+        String value;
+
+        public JSONString(String value) {
+            this.value = value;
+        }
+    }
 }
--- src/test/java/hu/adamsan/jsonparser/JSONTest.java
+++ src/test/java/hu/adamsan/jsonparser/JSONTest.java
@@ -2,6 +2,7 @@ package hu.adamsan.jsonparser;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class JSONTest {
@@ -11,5 +12,12 @@ class JSONTest {
         assertThrows(
                 NoSuchMethodException.class,
                 JSON.class::getConstructor);
+    }
+
+    @Test
+    void assertJSONparseCanParseString() {
+        String string = "\"apple\"";
+        JSON.JSONString json = JSON.parser(string);
+        assertEquals(string, json.value);
     }
 }
