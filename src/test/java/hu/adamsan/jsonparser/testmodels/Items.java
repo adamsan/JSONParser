0459bccfff32fc3dd246ae15800fd552f4d2fdd9
Szentirmai-Schon Adam <adamsan3@gmail.com>
2022-08-07 01:08:57 +0200

    add test models Person, Items

---
+++ src/test/java/hu/adamsan/jsonparser/testmodels/Items.java
@@ -0,0 +1,12 @@
+package hu.adamsan.jsonparser.testmodels;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Items {
+    private String name;
+    private Double price;
+    private String type;
+}
---
+++ src/test/java/hu/adamsan/jsonparser/testmodels/Person.java
@@ -0,0 +1,16 @@
+package hu.adamsan.jsonparser.testmodels;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class Person {
+    private Long age;
+    private String firstName;
+    private String id;
+    private Items items;
+    private List<Long> lastName;
+}
